e4d6fb33522c6fa17e0e221ed2791c54c061aca5
4bpb <blakebein@gmail.com>
2021-04-07 14:09:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client = new Discord.Client();
 var log = require('./logger')
 var moment = require('moment'); // require
 const puppeteer = require('puppeteer-extra')
+const imagesToPdf = require("images-to-pdf")
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
 
@@ -43,8 +44,8 @@ async function checkCookie(){
 
 async function login(){
     await page.goto("https://www.chegg.com/writing/login/", {waitUntil: 'networkidle2'});
-    await page.type('#emailForSignIn', '', {delay: 100}); // Types slower, like a user enter username here
-    await page.type('#passwordForSignIn', '', {delay: 100}); // Types slower, like a user enter pass here
+    await page.type('#emailForSignIn', 'blakebein@gmail.com', {delay: 100}); // Types slower, like a user
+    await page.type('#passwordForSignIn', '4Craftmine()!', {delay: 100}); // Types slower, like a user
     await page.click('#eggshell-8 > form > div > div > div > footer > button'); // Types slower, like a user
 }
 
@@ -84,6 +85,50 @@ client.on('message', message => {
 
 
                 global.screenshot = await page.screenshot({path: 'test.png', fullPage: true});
+
+
+
+                let divselectortoremove= "body > div.chg-body.no-nav.no-subnav.header-nav > div:nth-child(4)";
+                await page.evaluate((sel) => {
+                    var elements = document.querySelectorAll(sel);
+                    for(var i=0; i< elements.length; i++){
+                        elements[i].parentNode.removeChild(elements[i]);
+                    }
+                }, divselectortoremove)
+
+
+
+                await page.waitForSelector('body > div.chg-body.no-nav.no-subnav.header-nav > div.chg-container.center-content > div.chg-container-content > div.chg-global-content > div > div.parent-container.question-headline > div.main-content.question-page > div.dialog-question > div.question.txt-small > div.txt-body.question-body.mod-parent-container > div.ugc-base.question-body-text');          // wait for the selector to load
+                const element = await page.$('body > div.chg-body.no-nav.no-subnav.header-nav > div.chg-container.center-content > div.chg-container-content > div.chg-global-content > div > div.parent-container.question-headline > div.main-content.question-page > div.dialog-question > div.question.txt-small > div.txt-body.question-body.mod-parent-container > div.ugc-base.question-body-text');        // declare a variable with an ElementHandle
+                await element.screenshot({path: 'question.png'}); // take screenshot element in puppeteer
+                     
+              
+              
+              
+              
+                await page.waitForSelector('body > div.chg-body.no-nav.no-subnav.header-nav > div.chg-container.center-content > div.chg-container-content > div.chg-global-content > div > div.parent-container.question-headline > div.main-content.question-page > div.dialog-question > div.answers-wrap > ul > li > div.answer.txt-small.mod-parent-container > div.txt-body.answer-body');          // wait for the selector to load
+                const answer = await page.$('body > div.chg-body.no-nav.no-subnav.header-nav > div.chg-container.center-content > div.chg-container-content > div.chg-global-content > div > div.parent-container.question-headline > div.main-content.question-page > div.dialog-question > div.answers-wrap > ul > li > div.answer.txt-small.mod-parent-container > div.txt-body.answer-body');        // declare a variable with an ElementHandle
+                await answer.screenshot({path: 'answer.png'}); // take screenshot element in puppeteer
+              
+
+                main('question.png','answer.png')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 const cookies = await page.cookies();
 
                 await fs.writeFileSync('./cookies.json', '', function(){console.log('Cookies Cleared')})
@@ -122,4 +167,25 @@ async function whoRequest(info){
 }
 
 
-client.login('');
+async function main(questionimage, answerimage){
+    let rnadomstring = makeid(5)
+    await imagesToPdf([questionimage, answerimage], rnadomstring+".pdf")
+    log('Made file :'+rnadomstring+'.pdf','ok')
+}
+
+
+function makeid(length) {
+    var result           = [];
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result.push(characters.charAt(Math.floor(Math.random() * 
+ charactersLength)));
+   }
+   return result.join('');
+}
+
+
+
+
+client.login('NjkzOTY5MDAxNzcyMDg5NDE1.XoEzHg.NI-fSJgBXdxscHbDojP_ElStoZ8');
